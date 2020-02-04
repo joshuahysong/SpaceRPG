fd00c99e319148cc9f0fcc27875de9424645718f
joshuahysong <joshuahysong@gmail.com>
2020-02-04 13:24:04 -0500

    Added resize event for hud

--- src/scenes/HudScene.ts
+++ src/scenes/HudScene.ts
@@ -18,9 +18,26 @@ export default class HudScene extends SceneBase {
     private menuButtons: Array<Phaser.GameObjects.Container> = [];
     private itemButtons: Array<Phaser.GameObjects.Container> = [];
 
+    private itemButtonMargin: integer = 10;
+    private itemButtonPadding: integer = 10;
+    private itemButtonItemSize: integer = 42;
+    private itemButtonHeight: integer = this.itemButtonItemSize + this.itemButtonPadding * 2 + this.itemFont.fontSize;
+
     public create() {
         this.createMenuButton();
         this.createItemButton();
+        this.bindEvents();
+    }
+
+    private bindEvents() {
+        this.scale.on('resize', function() {
+            for (let m = 0; m < this.menuButtons.length; m++) {
+                this.menuButtons[m].y = window.innerHeight - this.menuButtonHeight / 2;
+            }
+            for (let i = 0; i < this.itemButtons.length; i++) {
+                this.itemButtons[i].y = (window.innerHeight - this.itemButtonHeight / 2) - this.itemButtonMargin;
+            }
+        }, this);
     }
 
     private createMenuButton() {
@@ -33,8 +50,7 @@ export default class HudScene extends SceneBase {
         button.setStrokeStyle(1, Phaser.Display.Color.HexStringToColor('#333333').color);
         var buttonText = this.add.text(0, 0, 'Build', { fontFamily: this.menuFont.fontFamily, fontSize: this.menuFont.fontSize, color: this.menuFont.color });
         buttonText.setOrigin(0.5);
-        var container = this.add.container(
-            this.menuButtonWidth / 2, window.innerHeight - this.menuButtonHeight / 2, [ button, buttonText ]);    
+        var container = this.add.container(this.menuButtonWidth / 2, window.innerHeight - this.menuButtonHeight / 2, [ button, buttonText ]);    
         container.setSize(this.menuButtonWidth, this.menuButtonHeight);    
         container.setInteractive({ cursor: 'pointer' });
         container.on('pointerover', function() {
@@ -59,32 +75,28 @@ export default class HudScene extends SceneBase {
 
     private createItemButton() {
         let tiles = this.cache.json.get('itemData');
-        let margin = 10;
-        let padding = 10;    
-        let itemSize = 42;
-        let buttonHeight = itemSize + padding * 2 + this.itemFont.fontSize; 
-        let buttonWidth = buttonHeight;
+        let buttonWidth = this.itemButtonHeight;
         let fillColor = Phaser.Display.Color.HexStringToColor('#222222').color;
         let hoverColor = Phaser.Display.Color.HexStringToColor('#333333').color;
-        let tileY = (buttonHeight / 2 * -1) + itemSize / 2 + padding;
-        let textY = tileY + itemSize / 2 + this.itemFont.fontSize / 2 + padding / 2;
+        let tileY = (this.itemButtonHeight / 2 * -1) + this.itemButtonItemSize / 2 + this.itemButtonPadding;
+        let textY = tileY + this.itemButtonItemSize / 2 + this.itemFont.fontSize / 2 + this.itemButtonPadding / 2;
         for (let i = 0; i < tiles.length; i++) {
-            let button = this.add.rectangle(0, 0, buttonWidth, buttonHeight);
+            let button = this.add.rectangle(0, 0, buttonWidth, this.itemButtonHeight);
             button.setFillStyle(fillColor);      
             button.setStrokeStyle(1, Phaser.Display.Color.HexStringToColor('#333333').color); 
             button.alpha = 0.75;
             let tile = new Tile(this, 0, tileY, 'shipTiles', tiles[i]);
             tile.setOrigin(0.5);
-            tile.displayWidth = itemSize;
+            tile.displayWidth = this.itemButtonItemSize;
             tile.scaleY = tile.scaleX;
             var buttonText = this.add.text(0, textY, tiles[i].name, 
                 { fontFamily: this.itemFont.fontFamily, fontSize: this.itemFont.fontSize, color: this.itemFont.color });
             buttonText.setOrigin(0.5);
-            let containerX = (buttonWidth / 2 + this.menuButtonWidth + margin) + (i * (buttonWidth + margin));
+            let containerX = (buttonWidth / 2 + this.menuButtonWidth + this.itemButtonMargin) + (i * (buttonWidth + this.itemButtonMargin));
             var container = this.add.container(
-                containerX, (window.innerHeight - buttonHeight / 2) - margin, 
+                containerX, (window.innerHeight - this.itemButtonHeight / 2) - this.itemButtonMargin,
                 [ button, tile, buttonText ]);
-            container.setSize(buttonWidth, buttonHeight);    
+            container.setSize(buttonWidth, this.itemButtonHeight);
             container.setInteractive({ cursor: 'pointer' });
             container.on('pointerover', function() {
                 this.list[0].fillColor = hoverColor;
